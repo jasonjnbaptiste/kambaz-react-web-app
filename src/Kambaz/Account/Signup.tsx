5bd5acdd854f989e45ca8521e8c9474e1ca3bf54
Jason Jn Baptiste <jnbaptiste.j@northeastern.edu>
2025-02-06 02:56:40 -0500

    sign up page

--- src/Kambaz/Account/Signup.tsx
+++ src/Kambaz/Account/Signup.tsx
@@ -1,12 +1,20 @@
+import { Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
 export default function Signup() {
   return (
     <div id="wd-signup-screen">
       <h3>Sign up</h3>
-      <input placeholder="username" className="wd-username" /><br/>
-      <input placeholder="password" type="password" className="wd-password" /><br/>
-      <input placeholder="verify password" type="password" className="wd-password-verify" /><br/>
-      <Link  to="/Kambaz/Account/Profile" > Sign up </Link><br />
-      <Link  to="/Kambaz/Account/Signin" >Sign in</Link>
+      <Form.Control placeholder="username" 
+              id="wd-username" 
+              className="mb-2" />
+      <Form.Control placeholder="password" 
+              id="wd-password"
+              type="password" 
+              className="mb-2" />
+      <Link id="wd-signup-btn"
+            to="/Kambaz/Account/Profile"
+            className="btn btn-primary w-100 mb-2">
+            Sign up </Link>
+      <Link id="wd-signin-link" to="/Kambaz/Account/Signin">Signin</Link>
     </div>
 );}
