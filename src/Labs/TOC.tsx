48688e099c35f8a018452801bdec4e688fc88cae
Jason Jn Baptiste <jnbaptiste.j@northeastern.edu>
2025-02-06 02:46:04 -0500

    github link update

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -20,7 +20,7 @@ export default function TOC() {
         <Nav.Link href="#/Kambaz">Kambaz</Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link href="https://github.com/jannunzi">My GitHub</Nav.Link>
+        <Nav.Link href="https://github.com/jasonjnbaptiste/kanbas-react-web-app">My GitHub</Nav.Link>
       </Nav.Item>
     </Nav>
     );
