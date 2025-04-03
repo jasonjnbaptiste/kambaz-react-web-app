5f36531be54ab1520db83bb297e11d05faf499ce
Jason Jn Baptiste <jnbaptiste.j@northeastern.edu>
2025-04-03 16:28:04 -0400

    TOC links adjustment

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -28,7 +28,11 @@ export default function TOC() {
       </Nav.Item>
       <Nav.Item>
         <Nav.Link href="https://github.com/jasonjnbaptiste/kambaz-react-web-app/tree/a5" target="_blank">React GitHub</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
         <Nav.Link href="https://github.com/jasonjnbaptiste/kambaz-node-server-app/tree/a5" target="_blank">Node GitHub</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
         <Nav.Link href="https://kambaz-node-server-app-lzzb.onrender.com/" target="_blank">Render Server</Nav.Link>
       </Nav.Item>
     </Nav>
