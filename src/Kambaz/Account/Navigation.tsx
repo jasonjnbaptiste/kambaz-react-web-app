710b784239a4f0840e704e3d44eaf20a524ac8d5
Jason Jn Baptiste <jnbaptiste.j@northeastern.edu>
2025-04-16 16:52:11 -0400

    6.3

--- src/Kambaz/Account/Navigation.tsx
+++ src/Kambaz/Account/Navigation.tsx
@@ -3,15 +3,15 @@ import { useSelector } from "react-redux";
 import { ListGroup } from "react-bootstrap";
 export default function AccountNavigation() {
   const { currentUser } = useSelector((state: any) => state.accountReducer);
-  const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
+  const links = currentUser ? ["Profile", "Users"] : ["Signin", "Signup"];
   const { pathname } = useLocation();
   return (
     links.map((link) => (
       <ListGroup id="wd-account-navigation">
-        <ListGroup.Item key={`/Kambaz/Account/${link}`} as={Link} to={`/Kambaz/Account/${link}`}
+        {(link == "Profile" || (currentUser && currentUser.role === "ADMIN")) && (<ListGroup.Item key={`/Kambaz/Account/${link}`} as={Link} to={`/Kambaz/Account/${link}`}
           className={`${pathname.includes(link) ? "text-black" : "text-danger"} border border-0`}>
           {link}
-        </ListGroup.Item>
+        </ListGroup.Item>)}
       </ListGroup>
     ))
   );
